22b91619f65d7fef521004de9d612675c5ab689b
Sean Bates <sean_b@live.com>
2018-03-01 10:03:18 -0600

    Refactor state right now

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserSearchComponent } from './user-search/user-search.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserService } from './user.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    UserSearchComponent,
+    UserDetailComponent,
+    DashboardComponent,
+    ProjectsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
