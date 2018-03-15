5a05ee7514bb569a9f577c2ccf7296f41b9fa705
Sean Bates <31301227+sBates21@users.noreply.github.com>
2018-03-14 23:21:40 -0500

    Moved the edit button to the general consultant list Added Region to general list added language proficiencies to details. Not saving properly

--- src/app/consultant-edit/consultant-edit.component.ts
+++ src/app/consultant-edit/consultant-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import {getCollection} from '@angular/cli/utilities/schematics';
 
 @Component({
   selector: 'app-consultant-edit',
@@ -27,11 +28,25 @@ export class ConsultantEditComponent implements OnInit {
   updateConsultant(id, consultant) {
     this.http.put('/consultant/' + id, this.consultant)
       .subscribe(res => {
-          let id = res['_id'];
+          const id = res['_id'];
           this.router.navigate(['/consultants']);
         }, (err) => {
           console.log(err);
         }
       );
+  }
+
+  addLanguageRow() {
+    const tableRef = document.getElementById('languageTable') as HTMLTableElement;
+    console.log(tableRef);
+    const newRow = tableRef.insertRow();
+    console.log('Hello!');
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+
+    cell1.innerHTML = 'Hello';
+    cell2.innerHTML = 'there';
+    cell3.innerHTML = 'General Kenobi';
   }
 }
