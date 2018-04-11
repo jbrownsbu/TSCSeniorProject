2f578d15e8e210f2a5fcdbb4c82f5633357d83ed
Jordan Brown <jbrown7061@gmail.com>
2018-04-10 19:32:08 -0500

    Pulling and displaying language and roles for consultant matching.

--- src/app/assign-consultant/assign-consultant.component.ts
+++ src/app/assign-consultant/assign-consultant.component.ts
@@ -13,6 +13,8 @@ export class AssignConsultantComponent implements OnInit {
   assignment = {};
   consultants: any[];
   selectedConsultant = {};
+  selectedConsultantLanguage = {};
+  selectedConsultantRoles: string[];
 
   constructor(private _location: Location, private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
 
@@ -32,12 +34,10 @@ export class AssignConsultantComponent implements OnInit {
       const dataLength = data['length'];
 
       let numMatches = 0;
-      let i;
-      let k;
-      for (i = 0; i < dataLength; i++) {
+      for (let i = 0; i < dataLength; i++) {
         let hasLanguage = false;
         const numLanguages = data[(i).toString()]['proficiencies'].length;
-        for (k = 0; k < numLanguages; k++) {
+        for (let k = 0; k < numLanguages; k++) {
           if (data[(i).toString()]['proficiencies'][(k).toString()]['language'] === this.assignment['language']) {
             hasLanguage = true;
           }
@@ -47,28 +47,82 @@ export class AssignConsultantComponent implements OnInit {
         }
       }
 
-      let j = 0;
-
+      let matchedConsultants = 0;
       this.consultants = new Array(numMatches);
-      for ( i = 0; i < dataLength; i++) {
+      for (let i = 0; i < dataLength; i++) {
+        let numRoles = 0;
+        if (this.assignment['isAudioToAudioRole'] === true && data[(i).toString()]['isAudioToAudioRole'] === true) {
+          numRoles++;
+        }
+        if (this.assignment['isGuestScholarRole'] === true && data[(i).toString()]['isGuestScholarRole'] === true) {
+          numRoles++;
+        }
+        if (this.assignment['isLinguisticConsultantRole'] === true && data[(i).toString()]['isLinguisticConsultantRole'] === true) {
+          numRoles++;
+        }
+        if (this.assignment['isManagerRole'] === true && data[(i).toString()]['isManagerRole'] === true) {
+          numRoles++;
+        }
+        if (this.assignment['isStoryCheckerRole'] === true && data[(i).toString()]['isStoryCheckerRole'] === true) {
+          numRoles++;
+        }
+        if (this.assignment['isTranslationConsultantInTrainingRole'] === true && data[(i).toString()]['isTranslationConsultantInTrainingRole'] === true) {
+          numRoles++;
+        }
+        if (this.assignment['isTranslationConsultantRole'] === true && data[(i).toString()]['isTranslationConsultantRole'] === true) {
+          numRoles++;
+        }
+
         let hasLanguage = false;
         const numLanguages = data[(i).toString()]['proficiencies'].length;
-        for (k = 0; k < numLanguages; k++) {
+        for (let k = 0; k < numLanguages; k++) {
           if (data[(i).toString()]['proficiencies'][(k).toString()]['language'] === this.assignment['language']) {
             hasLanguage = true;
           }
         }
         if (data[(i).toString()]['translationRegion'] === this.assignment['translationRegion'] && hasLanguage) {
-          this.consultants[j] = data[i.toString()];
-          j++;
+          this.consultants[matchedConsultants] = data[i.toString()];
+          matchedConsultants++;
         }
       }
+      this.getConsultant(this.consultants[0]['_id']);
     });
   }
 
   getConsultant(id) {
     this.http.get('consultant/' + id).subscribe(data => {
       this.selectedConsultant = data;
+      const numLanguages = data['proficiencies'].length;
+      let i;
+      for (i = 0; i < numLanguages; i++) {
+        if (data['proficiencies'][(i).toString()]['language'] === this.assignment['language']) {
+          this.selectedConsultantLanguage = data['proficiencies'][(i).toString()];
+        }
+      }
+
+      this.selectedConsultantRoles = new Array(0)
+      if (this.assignment['isAudioToAudioRole'] === true && data['isAudioToAudioRole'] === true) {
+        this.selectedConsultantRoles.push('Audio to Audio Translation Consultant');
+      }
+      if (this.assignment['isGuestScholarRole'] === true && data['isGuestScholarRole'] === true) {
+        this.selectedConsultantRoles.push('Guest Scholar (Exegetical, Linguistic, Bible)');
+      }
+      if (this.assignment['isLinguisticConsultantRole'] === true && data['isLinguisticConsultantRole'] === true) {
+        this.selectedConsultantRoles.push('Linguistic Consultant');
+      }
+      if (this.assignment['isManagerRole'] === true && data['isManagerRole'] === true) {
+        this.selectedConsultantRoles.push('Manager - Translation Consultant');
+      }
+      if (this.assignment['isStoryCheckerRole'] === true && data['isStoryCheckerRole'] === true) {
+        this.selectedConsultantRoles.push('Story Checker');
+      }
+      if (this.assignment['isTranslationConsultantInTrainingRole'] === true && data['isTranslationConsultantInTrainingRole'] === true) {
+        this.selectedConsultantRoles.push('Translation CiT');
+      }
+      if (this.assignment['isTranslationConsultantRole'] === true && data['isTranslationConsultantRole'] === true) {
+        this.selectedConsultantRoles.push('Translation Consultant');
+      }
+
       this.assignment['consultantId'] = data['_id'];
       this.assignment['consultantName'] = data['firstName'] + ' ' + data['lastName'];
     });
@@ -83,5 +137,4 @@ export class AssignConsultantComponent implements OnInit {
         }
       );
   }
-
 }
