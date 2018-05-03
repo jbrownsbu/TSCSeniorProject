ae1c3f5f3cbf69b64b67a799f679e53b75776821
Jordan Brown <jbrown7061@gmail.com>
2018-05-03 11:37:21 -0500

    Added sorting to all columns of projects list.

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -15,6 +15,15 @@ export class ProjectComponent implements OnInit {
 
   projects: any; // Holds project data after retrieved from database.
 
+  order = 'projectName';
+  reverse = false;
+  setOrder(value: string) {
+    if (this.order === value) {
+      this.reverse = !this.reverse;
+    }
+    this.order = value;
+  }
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
